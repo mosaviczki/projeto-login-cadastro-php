5fb528e3e1ce38df09f165330d6a8a8051c4f3b9
Weiry Gonçalves <33106370+weirygon@users.noreply.github.com>
2022-10-04 05:59:30 +0000

    Fixed CSS

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+require_once 'classes/usuarios.php';
+$u = new Usuario;
+?>
+<html lang="pt-BR">
+
+<head>
+    <link rel="stylesheet" type="text/css" href="CSS/style.css">
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+
+<body>
+    <div class="main_forms">
+        <div class="left_forms">
+            <img src="imagem/logo.png" alt="logo" width="100">
+            <img src="imagem/Movie Night-amico.svg">
+        </div>
+        <div class=right_forms>
+            <div class="body_forms">
+                <h1>ENTRAR</h1>
+                <form method="POST">
+                    <input type="email" placeholder="Usuário" name=email maxlenght="30">
+                    <input type="password" placeholder="Senha" name=senha maxlenght="30">
+                    <input type="submit" value="ACESSAR" maxlenght="30">
+                    <a href="cadastrar.php">Ainda não é inscrito? <strong> Cadastre-se</strong>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <?php
+    //verificar se clicou no botão
+    if (isset($_POST['email'])) {
+
+        $email = addslashes($_POST['email']);
+        $senha = addslashes($_POST['senha']);
+
+        //verificação de campo em branco
+        if (!empty($email) && !empty($senha)) {
+            $u->conectar("projeto_login", "localhost", "root", "");
+            if ($u->msgErro == "") {
+                if ($u->logar($email, $senha)) {
+                    header("location: ../site/index.php");
+                } else {
+                    ?>
+                    <div class="msg-erro">
+                        Email e/ou senha incorretos.
+                    </div>
+                    <?php
+                }
+            } else {
+                ?>
+                <div class="msg-erro">
+                    <?php echo "Erro: " . $u->$msgErro; ?>
+                </div>
+            <?php
+            }
+        } else {
+            ?>
+            <div class="msg-erro">
+                Preencha todos os campos!
+            </div>
+    <?php
+        }
+    }
+    ?>
+</body>
+
+</html>
