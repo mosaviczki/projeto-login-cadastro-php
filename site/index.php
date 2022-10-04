bf3e3a3ff4397eb0074c0310d1a29b98a3db8c05
mosaviczki <monique_saviczki@hotmail.com>
2022-10-04 00:49:50 -0300

    header modification

--- site/index.php
+++ site/index.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     if(!isset($_SESSION['id_usuario'])){
-        header("location: index.php");
+        header("location: ../login-cadastro/index.php");
         exit;
     }
 ?>
